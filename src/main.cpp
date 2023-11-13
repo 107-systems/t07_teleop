73abf4efd0f222d9dff9c1f9d6e09d891a716694
Alexander Entinger <consulting@lxrobotics.com>
2023-11-13 10:41:54 +0100

    Integrate everything into one loop.

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,19 @@
 
 #include <rclcpp/rclcpp.hpp>
 
-#include <t07_teleop/Node.h>
+#include <std_msgs/msg/float32.hpp>
+
+#include <mp-units/systems/si/si.h>
+
+#include <opencv2/highgui.hpp>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+using namespace mp_units;
+using mp_units::si::unit_symbols::m;
+using mp_units::si::unit_symbols::s;
 
 /**************************************************************************************
  * MAIN
@@ -20,11 +32,80 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
 
-  auto node = std::make_shared<t07_teleop::Node>();
+  auto node = rclcpp::Node::make_shared("t07_teleop_node");
+
+
+  node->declare_parameter("motor_left_topic", "/motor/left/target");
+  node->declare_parameter("motor_left_topic_deadline_ms", 100);
+  node->declare_parameter("motor_left_topic_liveliness_lease_duration", 1000);
+
+  auto const motor_left_topic = node->get_parameter("motor_left_topic").as_string();
+  auto const motor_left_topic_deadline = std::chrono::milliseconds(node->get_parameter("motor_left_topic_deadline_ms").as_int());
+  auto const motor_left_topic_liveliness_lease_duration = std::chrono::milliseconds(node->get_parameter("motor_left_topic_liveliness_lease_duration").as_int());
+
+  rclcpp::QoS motor_left_qos_profile{rclcpp::KeepLast(1), rmw_qos_profile_sensor_data};
+  motor_left_qos_profile.deadline(motor_left_topic_deadline);
+  motor_left_qos_profile.liveliness(RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC);
+  motor_left_qos_profile.liveliness_lease_duration(motor_left_topic_liveliness_lease_duration);
+
+  auto const motor_left_pub = node->create_publisher<std_msgs::msg::Float32>(motor_left_topic, motor_left_qos_profile);
+
+
+  node->declare_parameter("motor_right_topic", "/motor/right/target");
+  node->declare_parameter("motor_right_topic_deadline_ms", 100);
+  node->declare_parameter("motor_right_topic_liveliness_lease_duration", 1000);
+
+  auto const motor_right_topic = node->get_parameter("motor_right_topic").as_string();
+  auto const motor_right_topic_deadline = std::chrono::milliseconds(node->get_parameter("motor_right_topic_deadline_ms").as_int());
+  auto const motor_right_topic_liveliness_lease_duration = std::chrono::milliseconds(node->get_parameter("motor_right_topic_liveliness_lease_duration").as_int());
+
+  rclcpp::QoS motor_right_qos_profile{rclcpp::KeepLast(1), rmw_qos_profile_sensor_data};
+  motor_right_qos_profile.deadline(motor_right_topic_deadline);
+  motor_right_qos_profile.liveliness(RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC);
+  motor_right_qos_profile.liveliness_lease_duration(motor_right_topic_liveliness_lease_duration);
+
+  auto const motor_right_pub = node->create_publisher<std_msgs::msg::Float32>(motor_right_topic, motor_right_qos_profile);
 
   try
   {
-    rclcpp::spin(node);
+    quantity<m/s> motor_left_target  = 0. * m/s;
+    quantity<m/s> motor_right_target = 0. * m/s;
+
+    while(rclcpp::ok())
+    {
+      /* Wait for a key to be pressed. */
+      int const ch = cv::waitKey(100);
+
+      /* Change target velocities depending on keyboard input. */
+      if (ch >= 0)
+      {
+        if (tolower(ch) == 'w')
+        {
+          motor_left_target  += 0.1 * m/s;
+          motor_right_target += 0.1 * m/s;
+        }
+        else if (tolower(ch) == 's')
+        {
+          motor_left_target  -= 0.1 * m/s;
+          motor_right_target -= 0.1 * m/s;
+        }
+      }
+
+      /* Publish the target velocities. */
+      {
+        std_msgs::msg::Float32 motor_left_target_msg;
+        motor_left_target_msg.data = motor_left_target.numerical_value_in(m/s);
+        motor_left_pub->publish(motor_left_target_msg);
+      }
+      {
+        std_msgs::msg::Float32 motor_right_target_msg;
+        motor_right_target_msg.data = motor_right_target.numerical_value_in(m/s);
+        motor_right_pub->publish(motor_right_target_msg);
+      }
+
+      /* Let ROS do its things. */
+      rclcpp::spin_some(node);
+    }
     rclcpp::shutdown();
     return EXIT_SUCCESS;
   }
